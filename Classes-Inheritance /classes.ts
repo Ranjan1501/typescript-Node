dc55d09c94daef56a039afa6b45b7b92017a6203
Ranjan Kumar Thakur <ranjankumar1757@gmail.com>
2022-07-16 00:21:53 +0530

    class-modifiers

--- Classes-Inheritance /classes.ts
+++ Classes-Inheritance /classes.ts
@@ -1,11 +1,39 @@
+// Instance Method Modifier  
+// private public protected
 class Vehicle {
-    drive (): void {
-        console.log("Hey I am Ranjan");
+ 
+    constructor (public color: string) {
+        // note modifier can be used for variables also like color property
+        
     }
-    honk (): void {
+
+   protected honk (): void {
         console.log("No honk please!");
     } 
 }
-const vehicle = new Vehicle();
-vehicle.honk(); 
-vehicle.drive();
+const vehicle = new Vehicle("yellow" );
+console.log(vehicle.color); 
+
+// Public -- it can  called anywhere anytime you want. 
+// Private -- it can be called by other methods in this class. 
+// protected -- This method can be called by other methods in this class or other methods in child classes. 
+class Car extends Vehicle {
+    // this is a way to initialize a variable inside the class. 
+    constructor (public wheels: number, color: string) {
+        super(color); 
+    }
+    private drive (): void {
+            console.log("vroom")
+    }
+    startDrivingProcess (): void {
+        this.drive(); 
+        this.honk(); 
+        // it can be accessed by other methods in this class or in child classes. 
+    }
+
+
+}
+const car = new Car(4, "grey");
+car.startDrivingProcess(); 
+console.log(car.color); 
+console.log(car.wheels)
